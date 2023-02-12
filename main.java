c4b95ed7c97fafabb18ce4aa6cb8bd5faee3559f
bder00s <105774363+bder00s@users.noreply.github.com>
2023-02-12 12:57:18 +0100

    opdr 1-3

--- main.java
+++ main.java
@@ -3,17 +3,40 @@
 public class main {
     public static void main(String[] args) {
 
-        int number;
+        int number = 0;
         int input1;
         int input2;
         String name;
 
         hello();
 
+        System.out.println(positiveOrNegative(number));
+
+        System.out.println(positiveZeroOrNegative(number));
+
+
 
     }
 
     public static void hello(){
         System.out.println("Hello world");
+    }
+
+    public static String positiveOrNegative(int number){
+        if (number > 0 ){
+            return "This number is positive!";
+        } else {
+            return "This number is negative!";
+        }
+    }
+
+    public static String positiveZeroOrNegative(int number) {
+        if (number > 0) {
+            return "This number is positive!";
+        } else if (number < 0){
+            return "This number is negative!";
+        } else {
+            return "This number is zero";
+        }
     }
 }
