36d38e7dad6510980ceae54268af3a360501d8fd
bder00s <105774363+bder00s@users.noreply.github.com>
2023-02-12 12:46:23 +0100

    Opzet HW opdracht 1

---
+++ main.java
@@ -0,0 +1,19 @@
+
+
+public class main {
+    public static void main(String[] args) {
+
+        int number;
+        int input1;
+        int input2;
+        String name;
+
+        hello();
+
+
+    }
+
+    public static void hello(){
+        System.out.println("Hello world");
+    }
+}
